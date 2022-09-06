681d2ccbb4b1226d58ca3241b5bf478862cbb41f
iheb-athimni <34520201+iheb-athimni@users.noreply.github.com>
2022-09-06 17:25:41 +0100

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -3,10 +3,21 @@ package Runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources/Features"},glue ="StepsDefinition",tags = "@testAll")
 
 public class TestRunner {
+    @Test
+    void sendMailTest() throws IOException {
+//        String rapportPath;
+        SendMailFromOutlook sendTheMail = new SendMailFromOutlook();
+//        zipClass zipClasstest = new zipClass();
+//        rapportPath = zipClasstest.executeTasks();
+        sendTheMail.main();
+
+        Karate.run("LoginAPI").relativeTo(getClass());
+    }
 
 }
