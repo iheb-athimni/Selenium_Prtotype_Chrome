bccd95c2ba7d4efa60919baacc511ff4bf0b300a
iheb-athimni <34520201+iheb-athimni@users.noreply.github.com>
2022-09-09 11:02:54 +0100

    Update Constants.java

--- src/test/java/Runner/Constants.java
+++ src/test/java/Runner/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String FULL_PATTERN_DATE = "dd-MM-yyyy HH:mm:ss";
     public static final String MAIL_SMTP_HOST = "mail.smtp.host";
     public static final String MAIL_SMTP_PORT = "mail.smtp.port";
-    public static final String Project_Front_Name = "Automation-Test-UI";
+    public static final String Project_Front_Name = "Automation-Test-UI-chrome-browser";
     public static final String Rapport_Path_Test = "target/karate-reports/";
     public static final String Rapport_Path_Test2 = "target/reportsToArchive/";
 
