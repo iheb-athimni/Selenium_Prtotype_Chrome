7552c04b5d1b11ef3f14f0b910dbf0218036af9c
TestingLab <34520201+ihebTestingLab@users.noreply.github.com>
2022-12-17 23:43:38 +0100

    add report

--- src/test/java/Runner/Constants.java
+++ src/test/java/Runner/Constants.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Constants {
+    public static final String Rapport_Path = "target/reportsToArchive/";
     public static final String PATTERN_DATE = "dd-MM-yyyy";
     public static final String FULL_PATTERN_DATE = "dd-MM-yyyy HH:mm:ss";
 }
