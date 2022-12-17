ef6f4d81c7d297f25bcefca6dacb8ecb0830e8de
TestingLab <34520201+ihebTestingLab@users.noreply.github.com>
2022-12-17 22:18:29 +0100

    Update Constants.java

--- src/test/java/Runner/Constants.java
+++ src/test/java/Runner/Constants.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Constants {
-    public static final String Rapport_Path = "target/reportsToArchive/";
     public static final String PATTERN_DATE = "dd-MM-yyyy";
     public static final String FULL_PATTERN_DATE = "dd-MM-yyyy HH:mm:ss";
-    public static final String Project_Front_Name = "Automation-Test-UI-chrome-browser";
 }
